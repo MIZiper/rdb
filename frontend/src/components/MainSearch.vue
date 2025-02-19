c72d698252537a4bd95e93b9aece6c3ba939af7b
MIZiper <miziper@163.com>
2025-02-19 19:01:34 +0000

    merge two fetches, combine static and dynamic mode

--- frontend/src/components/MainSearch.vue
+++ frontend/src/components/MainSearch.vue
@@ -1,9 +1,9 @@
 <template>
-    <TagAdder class="pb-0" ref="tagAdder"/>
-    <v-container class="pt-0">
-        <v-btn prepend-icon="mdi-magnify" @click="searchResources">Search</v-btn>
-    </v-container>
-    <ResourceList ref="resourceList"/>
+  <TagAdder class="pb-0" ref="tagAdder" />
+  <v-container class="pt-0">
+    <v-btn prepend-icon="mdi-magnify" @click="searchResourcesByTags">Search</v-btn>
+  </v-container>
+  <ResourceList ref="resourceList" />
 </template>
 
 <script>
@@ -16,25 +16,30 @@ export default {
     ResourceList
   },
   methods: {
-    async searchResources() {
+    async searchResourcesByTags() {
       const selectedTags = this.$refs.tagAdder.selectedTags;
       try {
         const response = await fetch(`http://localhost:5428/resources?tags=${selectedTags.join(';;')}`);
         const data = await response.json();
-        this.$refs.resourceList.results = data['resources'].map(resource => ({
-          title: resource.name,
-          description: resource.description || '',
-          tags: resource.tags,
-          modifiedDate: resource.modifiedDate
-        }));
-        this.$refs.resourceList.totalItems = data['total_resources'];
-        this.$refs.resourceList.page = 1;
-        this.$refs.resourceList.itemsPerPage = 7;
-        this.$refs.resourceList.paginationMode = 'static';
+        // data['items_per_page'] can be [0, int)
+        this.$refs.resourceList.setResults(data, this.fetchResources, 1);
       } catch (error) {
         console.error('Error fetching resources:', error);
       }
-    }
+    },
+    async fetchResources(page = 1) {
+      try {
+        const response = await fetch(`http://localhost:5428/resources?page=${page}`);
+        const data = await response.json();
+        // data['items_per_page'] > 0
+        this.$refs.resourceList.setResults(data, this.fetchResources, page);
+      } catch (error) {
+        console.error('Error fetching resources:', error);
+      }
+    },
+  },
+  created() {
+    this.fetchResources();
   }
 };
 </script>
--- frontend/src/components/ResourceList.vue
+++ frontend/src/components/ResourceList.vue
@@ -5,7 +5,8 @@
         <p>{{ totalItems }} results found</p>
       </v-col>
       <v-col cols="auto" class="d-flex align-center">
-        <v-pagination v-model="page" :length="pageCount" :total-visible="5" @update:modelValue="onPageChange"></v-pagination>
+        <v-pagination v-model="page" :length="pageCount" :total-visible="5"
+          @update:modelValue="onPageChange"></v-pagination>
         <v-btn icon @click="toggleSortOrder" class="ml-3">
           <v-icon>
             {{
@@ -21,27 +22,28 @@
     </v-row>
     <v-list>
       <v-list-item v-for="(result, index) in paginatedResults" :key="index" class="mb-2">
-          <v-row>
-            <v-col>
-              <v-list-item-title>{{ result.title }}</v-list-item-title>
-            </v-col>
-            <v-col cols="auto">
-              <v-icon small class="mr-1">mdi-calendar</v-icon>
-              <small>{{ result.modifiedDate }}</small>
-            </v-col>
-          </v-row>
-          <v-list-item-subtitle class="ma-3">{{ result.description }}</v-list-item-subtitle>
-          <v-row class="ma-0">
-            <a v-for="(tag, tagIndex) in result.tags" :key="tagIndex" :href="`/search?tag=${tag}`" target="_blank">
-              <v-chip :color="getTagColor(tag)" class="mr-1 mb-1">
-                {{ tag }}
-              </v-chip>
-            </a>
-          </v-row>
+        <v-row>
+          <v-col>
+            <v-list-item-title>{{ result.title }}</v-list-item-title>
+          </v-col>
+          <v-col cols="auto">
+            <v-icon small class="mr-1">mdi-calendar</v-icon>
+            <small>{{ result.modifiedDate }}</small>
+          </v-col>
+        </v-row>
+        <v-list-item-subtitle class="ma-3">{{ result.description }}</v-list-item-subtitle>
+        <v-row class="ma-0">
+          <a v-for="(tag, tagIndex) in result.tags" :key="tagIndex" :href="`/search?tag=${tag}`" target="_blank">
+            <v-chip :color="getTagColor(tag)" class="mr-1 mb-1">
+              {{ tag }}
+            </v-chip>
+          </a>
+        </v-row>
       </v-list-item>
     </v-list>
     <v-row justify="end">
-      <v-pagination v-model="page" :length="pageCount" :total-visible="5" @update:modelValue="onPageChange"></v-pagination>
+      <v-pagination v-model="page" :length="pageCount" :total-visible="5"
+        @update:modelValue="onPageChange"></v-pagination>
     </v-row>
   </v-container>
 </template>
@@ -52,79 +54,12 @@ import { getTagColor } from './utils'; // Import the getTagColor function
 export default {
   data() {
     return {
-      results: [
-        {
-          title: 'Sample Result 1',
-          description: 'Description for Sample Result 1',
-          tags: ['apple', 'banana'],
-          modifiedDate: '2023-10-01'
-        },
-        {
-          title: 'Sample Result 2',
-          description: 'Description for Sample Result 2',
-          tags: ['cherry', 'date'],
-          modifiedDate: '2023-10-02'
-        },
-        {
-          title: 'Sample Result 3',
-          description: 'Description for Sample Result 3',
-          tags: ['elderberry', 'fig'],
-          modifiedDate: '2023-10-03'
-        },
-        {
-          title: 'Sample Result 4',
-          description: 'Description for Sample Result 4',
-          tags: ['grape', 'honeydew'],
-          modifiedDate: '2023-10-04'
-        },
-        {
-          title: 'Sample Result 5',
-          description: 'Description for Sample Result 5',
-          tags: ['kiwi', 'lemon'],
-          modifiedDate: '2023-10-05'
-        },
-        {
-          title: 'Sample Result 6',
-          description: 'Description for Sample Result 6',
-          tags: ['mango', 'nectarine'],
-          modifiedDate: '2023-10-06'
-        },
-        {
-          title: 'Sample Result 7',
-          description: 'Description for Sample Result 7',
-          tags: ['orange', 'papaya'],
-          modifiedDate: '2023-10-07'
-        },
-        {
-          title: 'Sample Result 8',
-          description: 'Description for Sample Result 8',
-          tags: ['quince', 'raspberry'],
-          modifiedDate: '2023-10-08'
-        },
-        {
-          title: 'Sample Result 9',
-          description: 'Description for Sample Result 9',
-          tags: ['strawberry', 'tangerine'],
-          modifiedDate: '2023-10-09'
-        },
-        {
-          title: 'Sample Result 10',
-          description: 'Description for Sample Result 10',
-          tags: ['ugli', 'vanilla'],
-          modifiedDate: '2023-10-10'
-        },
-        {
-          title: 'Sample Result 11',
-          description: 'Description for Sample Result 11',
-          tags: ['watermelon', 'xigua'],
-          modifiedDate: '2023-10-11'
-        },
-      ],
+      results: [],
       page: 1,
       totalItems: 0,
       itemsPerPage: 7,
       sortOrder: 'None',
-      paginationMode: 'static' // 'dynamic' mode for resources listing, 'static' mode for results filtered by tags
+      paginationCB: null,
     };
   },
   methods: {
@@ -137,40 +72,110 @@ export default {
         this.results.sort((a, b) => new Date(b.modifiedDate) - new Date(a.modifiedDate));
       }
     },
-    async fetchResources() {
-      try {
-        const response = await fetch(`http://localhost:5428/resources?page=${this.page}`);
-        const data = await response.json();
-        this.results = data['resources'].map(resource => ({
-          title: resource.name,
-          description: resource.description || '',
-          tags: resource.tags,
-          modifiedDate: resource.modifiedDate
-        }));
-        this.totalItems = data['total_resources'];
+    setResults(data, dynamic_cb = null, page = 1) {
+      this.results = data['resources'].map(resource => ({
+        title: resource.name,
+        description: resource.description || '[No description]',
+        tags: resource.tags,
+        modifiedDate: resource.modifiedDate
+      }));
+      this.totalItems = data['total_resources'];
+      this.page = page;
+      if (data['items_per_page'] == 0) {
+        this.itemsPerPage = 7;
+        this.paginationCB = null;
+      } else {
         this.itemsPerPage = data['items_per_page'];
-        this.paginationMode = 'dynamic';
-      } catch (error) {
-        console.error('Error fetching resources:', error);
+        this.paginationCB = dynamic_cb;
       }
     },
     getTagColor, // Add getTagColor to methods
     onPageChange() {
-      if (this.paginationMode === 'dynamic') {
-        this.fetchResources();
+      if (this.paginationCB) {
+        this.paginationCB(this.page);
       }
     }
   },
   created() {
-    this.totalItems = this.results.length;
-    this.fetchResources();
+    const example = [
+      {
+        name: 'Sample Result 1',
+        description: 'Description for Sample Result 1',
+        tags: ['apple', 'banana'],
+        modifiedDate: '2023-10-01'
+      },
+      {
+        name: 'Sample Result 2',
+        description: 'Description for Sample Result 2',
+        tags: ['cherry', 'date'],
+        modifiedDate: '2023-10-02'
+      },
+      {
+        name: 'Sample Result 3',
+        description: 'Description for Sample Result 3',
+        tags: ['elderberry', 'fig'],
+        modifiedDate: '2023-10-03'
+      },
+      {
+        name: 'Sample Result 4',
+        description: 'Description for Sample Result 4',
+        tags: ['grape', 'honeydew'],
+        modifiedDate: '2023-10-04'
+      },
+      {
+        name: 'Sample Result 5',
+        description: 'Description for Sample Result 5',
+        tags: ['kiwi', 'lemon'],
+        modifiedDate: '2023-10-05'
+      },
+      {
+        name: 'Sample Result 6',
+        description: 'Description for Sample Result 6',
+        tags: ['mango', 'nectarine'],
+        modifiedDate: '2023-10-06'
+      },
+      {
+        name: 'Sample Result 7',
+        description: 'Description for Sample Result 7',
+        tags: ['orange', 'papaya'],
+        modifiedDate: '2023-10-07'
+      },
+      {
+        name: 'Sample Result 8',
+        description: 'Description for Sample Result 8',
+        tags: ['quince', 'raspberry'],
+        modifiedDate: '2023-10-08'
+      },
+      {
+        name: 'Sample Result 9',
+        description: 'Description for Sample Result 9',
+        tags: ['strawberry', 'tangerine'],
+        modifiedDate: '2023-10-09'
+      },
+      {
+        name: 'Sample Result 10',
+        description: 'Description for Sample Result 10',
+        tags: ['ugli', 'vanilla'],
+        modifiedDate: '2023-10-10'
+      },
+      {
+        name: 'Sample Result 11',
+        description: 'Description for Sample Result 11',
+        tags: ['watermelon', 'xigua'],
+        modifiedDate: '2023-10-11'
+      },
+    ];
+
+    this.setResults(
+      { resources: example, total_resources: example.length, items_per_page: 0 },
+    )
   },
   computed: {
     pageCount() {
       return Math.ceil(this.totalItems / this.itemsPerPage);
     },
     paginatedResults() {
-      if (this.paginationMode === 'static') {
+      if (!this.paginationCB) {
         const start = (this.page - 1) * this.itemsPerPage;
         const end = start + this.itemsPerPage;
         return this.results.slice(start, end);
