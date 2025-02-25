4e028b957797ed05490cc1fa15b00d708562371b
MIZiper <miziper@163.com>
2025-02-25 20:03:04 +0000

    to add a link to add new resource
    and then change the logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <v-app>
+    <v-app-bar color="secondary">
+      <a href="/">
+        <v-img src="./assets/logo-w.png" height="48" width="48" class="ml-4" />
+      </a>
+      <v-toolbar-title>Result database</v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+      <v-btn href="/resources/new" title="Add resource"><v-icon>mdi-plus</v-icon></v-btn>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
